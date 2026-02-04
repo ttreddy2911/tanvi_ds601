a1607d336145d2a07e79f4aaaebf506732dfffb2
NANDA KISHORE TIPPIREDDY <tt437@njit.edu>
2026-02-03 23:04:57 -0500

    myproject

---
+++ calculator.py
@@ -0,0 +1,19 @@
+def add(x, y):
+    """Adds two numbers."""
+    return x + y
+
+def subtract(x, y):
+    """Subtracts two numbers."""
+    return x - y
+
+def multiply(x, y):
+    """Multiplies two numbers."""
+    return x * y
+
+def divide(x, y):
+    """Divides two numbers."""
+    if y == 0:
+        # Raises a ValueError with the specific message
+        raise ValueError("Cannot divide by zero")
+    return x / y
+
